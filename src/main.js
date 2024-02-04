11eca3a0591ba4a00b4a72b3a9fdbfc6a50cb3f0
Daenges <daenges2@gmail.com>
2024-02-04 16:29:42 +0100

    Replace chromeapp with webtorrent.min.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
-import WebTorrent from './webtorrent/webtorrent.chromeapp'
+import WebTorrent from './webtorrent/webtorrent.min.js'
 
 module.exports = class MyPlugin {
     constructor(meta) {
     }
   
     start() {
+      const magnetURI = "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent"
       const client = new WebTorrent()
-      const magnetURI = '...'
-
       client.add(magnetURI, torrent => {
         // Got torrent metadata!
-        console.log('Client is downloading:', torrent.infoHash);
-      })
+        console.log('Client is downloading:', torrent.infoHash)}
+      );
     }
   
     stop() {
