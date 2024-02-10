faa0fd4eecff2a001cba19ff63a6ec8793ec6192
Daenges <daenges2@gmail.com>
2024-02-10 23:55:44 +0100

    Working Tailwind

--- src/META.js
+++
@@ -1,7 +0,0 @@
-/**
- * @name DiscordWebTorrent
- * @author Daenges
- * @description Circumvent upload restrictions through modern P2P technology.
- * @version 0.0.1
- */
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import WebTorrent from './webtorrent/webtorrent.min.js'
-import styles from 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './tailwind/output.css'
 
 
 const plugin = class MyPlugin {
@@ -8,9 +8,9 @@ const plugin = class MyPlugin {
     }
   
     start() {
-      BdApi.DOM.addStyle(this.meta.name, styles);
       const magnetURI = "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent"
       const client = new WebTorrent()
+      BdApi.DOM.addStyle(this.meta.name, styles);
       
       // client.add(magnetURI, torrent => {
       //   // Got torrent metadata!
@@ -19,13 +19,12 @@ const plugin = class MyPlugin {
     }
   
     stop() {
-      BdApi.DOM.removeStyle(this.meta.name);
+      BdApi.DOM.removeStyle(this.meta.name, styles);
       // Cleanup when disabled
     }
 
     getSettingsPanel() {
-      const mainDiv = <div id="DiscordWebtorrentSettingsPanel">Hallo Welt</div>
-      BdApi.DOM.addStyle("DiscordWebtorrentSettingsPanel", styles)
+      const mainDiv = <button type="button" class="bg-green-500">Primary</button>
       return mainDiv;
     }
 
