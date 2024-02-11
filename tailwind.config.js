9ad6ca918b5e6994a588b26d24e6001b5e677b82
Daenges <daenges2@gmail.com>
2024-02-11 22:10:31 +0100

    Add .tsx to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
   prefix: "dwt-tw-",
   theme: {
     extend: {},
