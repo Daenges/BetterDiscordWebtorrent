50fea3e20ab13aa13558b4458062e1a265b79d64
Daenges <daenges2@gmail.com>
2024-02-14 00:05:55 +0100

    First proper looking interface

--- src/table.tsx
+++ src/table.tsx
@@ -2,19 +2,29 @@
 
 interface GenerateTableProps {
     magnetToPathMap: string[][];
-  }
-  
+}
+
+interface FileEntryProps {
+    path: string[];
+}
+
+function FileEntry(props: FileEntryProps) {
+    return <div className="dwt-tw-mt-5 dwt-tw-grid dwt-tw-grid-cols-5 dwt-tw-grid-flow-col dwt-tw-gap-2">
+        {props.path.map((path) => <div className="dwt-tw-bg-neutral-900 dwt-tw-rounded-sm dwt-tw-col-span-2 dwt-tw-text-gray-400">{path}</div>)}
+        <button className="dwt-tw-bg-blue-500 hover:dwt-tw-bg-blue-600 dwt-tw-rounded-sm dwt-tw-tw-text-center dwt-tw-text-white">Edit</button>
+    </div>
+}
 
 export default function GenerateTable(props: GenerateTableProps) {
     return (
-    <table className="dwt-tw-bg-green-600">
-        {props.magnetToPathMap.map((path) => (
-        <tr>
-            <td>{path[0]}</td>
-            <td>{path[1]}</td>
-        </tr>
-        ))}
-    </table>
+        <div>
+            <span className="dwt-tw-font-bold dwt-tw-text-white">FILES</span>
+            <div id="itemList">
+                {props.magnetToPathMap.map((path) =>
+                    <FileEntry path={path} />
+                )}
+            </div>
+      </div>
     );
 }
   
