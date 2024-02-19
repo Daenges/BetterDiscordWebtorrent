7340392a2e3d53fdc6761f5c6b78fb08ceb6aa4a
Daenges <daenges2@gmail.com>
2024-02-19 23:18:13 +0100

    File dialogue, returning undefined

--- src/quickSendButton.tsx
+++ src/quickSendButton.tsx
@@ -6,18 +6,22 @@ export default class QuickSendButton {
     sendButton: JSX.Element;
 
     constructor() {
-        this.sendButton = <button onClick={() => BdApi.UI.showConfirmationModal("Ist Mayonnaise auch ein Instrument?", <div>...</div>, {
-            confirmText: "Ja, aber nur Dienstags.",
-            cancelText: "Nein."
-        })} className="hover:dwt-tw-text-xl dwt-tw-text-lg dwt-tw-duration-100 dwt-tw-bg-transparent">🧲</button>;
+        this.sendButton = <button onClick={() => this.CreateNewTorrent()} className="hover:dwt-tw-text-xl dwt-tw-text-lg dwt-tw-duration-100 dwt-tw-bg-transparent">🧲</button>;
         AddToEmojiBox("DiscordWebTorrent", this.sendButton); 
     }
 
-    initialize() {
-
-    }
-
-    unload() {
-
+    CreateNewTorrent() {
+        BdApi.UI.openDialog({}).then(result => {
+            if(!result.cancelled) {
+                console.log(result.filePath);
+                BdApi.UI.showConfirmationModal("Torrent Confirmation", 
+                <div className="dwt-tw-text-center">
+                    <span>Do you really want to share this file?</span>
+                    <br/>
+                    <span className="dwt-tw-bg-neutral-800 dwt-tw-p-2 dwt-tw-rounded-sm dwt-tw-text-rose-500">{result.filePath}</span>
+                </div>, 
+                {confirmText: "Share", onConfirm: () => {}});
+            }
+        });
     }
 }
--- src/table.tsx
+++ src/table.tsx
@@ -12,7 +12,7 @@ interface FileEntryProps {
 function FileEntry(props: FileEntryProps) {
     return <div className="dwt-tw-mt-5 dwt-tw-mr-2 dwt-tw-bg-neutral-800 dwt-tw-p-2 dwt-tw-rounded-md dwt-tw-grid dwt-tw-grid-flow-col dwt-tw-columns-5 dwt-tw-gap-2">
         <div className="dwt-tw-grid dwt-tw-grid-flow-col-dense dwt-tw-gap-2 dwt-tw-col-span-4">
-            {props.path.map((path) => <input className="dwt-tw-bg-neutral-900 dwt-tw-rounded-sm dwt-tw-text-gray-400 dwt-tw-p-1" value={path} disabled/>)}
+            {props.path.map((path) => <input className="dwt-tw-bg-neutral-900 dwt-tw-rounded-sm dwt-tw-text-gray-400 dwt-tw-border-0 dwt-tw-p-1" value={path} disabled/>)}
         </div>
         <button className="dwt-tw-bg-rose-500 hover:dwt-tw-bg-rose-600 dwt-tw-rounded-sm dwt-tw-tw-text-center dwt-tw-col-span-1 dwt-tw-text-white">Remove</button>
     </div>
